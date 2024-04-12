f6b1cb94fa8fc65053db1c9df7285ccee989f346
Shikha-tomar <shikha12techies@gmail.com>
2024-04-12 15:34:39 +0530

    this is my fisrt

---
+++ 4_BasicServer.js
@@ -0,0 +1,25 @@
+// const colors = require("colors");
+// console.log("helxclo".green)
+
+const chalk = require("chalk") 
+console.log("vhvjn".red)
+// _______single thraded h - ek time mai ek hi command run karti h
+
+// const http = require("http") //server ki req or res ko handle karta h
+
+
+// createServer always function as a perameter leta h
+// req= koi hamne file send ki to unko nodejs ke ander access karta h
+// res= and nodejs ki file se jo send karte h usko res bolte h
+
+
+// http.createServer((req, res)=>{
+//     res.write("hello this is shikha");
+//     res.end();
+
+// }).listen(4500);
+
+
+// Package.json ek metadata file hai jo har Node.js project ke root directory mein hoti hai. Is file mein project ke dependencies, version information, author information, scripts aur aur bhi bahut kuch define kiya ja sakta hai. Ye file NPM ke liye important hai, kyunki ismein project ke saare dependencies aur settings ko store kiya jata hai.
+// npm - 
+// NPM ka matlab hota hai Node Package Manager. Ye ek package manager hai Node.js ke liye, jo ki ek tool hai jo Node.js projects mein libraries aur dependencies ko manage karne ke liye istemal hota hai. NPM developers ko unke projects mein required packages aur unke dependencies ko aasani se install, update aur manage karne ki suvidha deta hai.
