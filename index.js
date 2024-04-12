2f0fcb617a69258e61b0054709e66bef0d024365
Shikha-tomar <shikha12techies@gmail.com>
2024-04-12 17:06:46 +0530

    second commit

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-// type nul > index.js
-
 const express = require('express');
-const {I18n} = require('i18n')
-const path = require('path')
+const { I18n } = require('i18n');
+const path = require('path');
 const app = express();
 const port = 3000;
 const i18n = new I18n({
-    locales:['en','hi'],
-    directory: path.join(__dirname,'translation'),
+    locales: ['en', 'hi', 'franch'],
+    directory: path.join(__dirname, 'translation'),
     defaultLocale: 'hi'
-})
+});
 
+app.use(i18n.init);
 
-app.use(i18n.init)
-app.get("/test", async(req, res)=>{
-    res.send({id:1, name: res.__('MESSAGE')});
-})
-app.listen(port, ()=>{
-    console.log(`started on port ${port}`);
-})
+app.get("/test", async (req, res) => {
+    res.send({ id: 1, name: res.__('MESSAGE') });
+});
 
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
